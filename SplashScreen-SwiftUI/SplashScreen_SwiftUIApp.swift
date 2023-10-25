047b0cbf8942e62c0fb07e7937abbe6e403b4222
Zaid Sheikh <zaid.shaikh@cubixlabs.com>
2023-10-25 14:06:03 +0500

    Initial Commit

---
+++ SplashScreen-SwiftUI/SplashScreen_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplashScreen_SwiftUIApp.swift
+//  SplashScreen-SwiftUI
+//
+//  Created by Zaid Sheikh on 25/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct SplashScreen_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
